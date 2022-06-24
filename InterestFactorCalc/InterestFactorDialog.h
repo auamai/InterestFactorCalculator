cda45eb78354d992a86f8c9f15a7dfec358e238d
auamai <ighodarouamai@gmail.com>
2022-06-24 01:29:31 -0700

    Add project files.

---
+++ InterestFactorCalc/InterestFactorDialog.h
@@ -0,0 +1,19 @@
+#ifndef INTERESTFACTORDIALOG_H
+#define INTERESTFACTORDIALOG_H
+
+#include<QtWidgets/QDialog>
+
+class InterestFactorDialog : public QDialog
+{
+	Q_OBJECT;
+public:
+
+signals:
+
+private slots:
+
+private:
+
+
+};
+#endif
---
+++ InterestFactorCalc/main.cpp
@@ -0,0 +1,9 @@
+#include<QtWidgets/QApplication>
+
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+	return app.exec();
+}
